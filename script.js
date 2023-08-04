2b1e4512e1a57609879f271d38d6caf1dfa998ce
itsmeodom <arraraza@gmail.com>
2023-08-04 11:35:25 -0700

    Update script.js
    Change the script to enable player name input and choosing the number of teams:

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
-const numRounds = 5;
-const numPlayers = 9;
-const playerNames = [...Array(numPlayers).keys()].map((i) => `Player ${i+1}`);
-const scores = playerNames.map(() => 0);
+let playerNames = [];
+let scores = [];
+
+document.getElementById('addPlayer').addEventListener('click', addPlayer);
+document.getElementById('startSetup').addEventListener('click', startSetup);
+document.getElementById('startGame').addEventListener('click', startGame);
+
+function addPlayer() {
+  const playerDiv = document.createElement('div');
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = 'Enter Player Name';
+  playerDiv.appendChild(input);
+  document.getElementById('playerNames').appendChild(playerDiv);
+}
+
+function startSetup() {
+  playerNames = [...document.getElementById('playerNames').querySelectorAll('input')].map(input => input.value);
+  scores = playerNames.map(() => 0);
+
+  document.getElementById('playerSetup').style.display = 'none';
+  document.getElementById('teamSetup').style.display = 'block';
+}
 
 function startGame() {
-    for (let i = 1; i <= numRounds; i++) {
-        createRound(i);
-    }
-    calculateTotalScores();
+  const numTeams = document.getElementById('numTeams').value;
+  for (let i = 1; i <= numRounds; i++) {
+    createRound(i, numTeams);
+  }
+  calculateTotalScores();
 }
 
-function createRound(roundNumber) {
-    const roundDiv = document.createElement('div');
-    roundDiv.innerHTML = `<h2>Round ${roundNumber}</h2>`;
-    document.getElementById('rounds').appendChild(roundDiv);
-
-    const teams = createRandomTeams();
-    teams.forEach((team, index) => {
-        const teamDiv = document.createElement('div');
-        teamDiv.innerHTML = `<h3>Team ${index+1}</h3>`;
-        roundDiv.appendChild(teamDiv);
-        
-        team.forEach(player => {
-            const scoreDiv = document.createElement('div');
-            const input = document.createElement('input');
-            input.type = 'number';
-            input.value = 0;
-            input.className = 'score';
-            input.addEventListener('input', (e) => updateScore(player, roundNumber, Number(e.target.value)));
-            teamDiv.appendChild(document.createTextNode(player));
-            teamDiv.appendChild(input);
-            teamDiv.appendChild(document.createElement('br'));
-        });
-    });
+function createRound(roundNumber, numTeams) {
+  // ...
+  const teams = createRandomTeams(numTeams);
+  // ...
 }
 
-function createRandomTeams() {
-    const shuffledPlayers = playerNames.sort(() => 0.5 - Math.random());
-    return [shuffledPlayers.slice(0, shuffledPlayers.length / 2), shuffledPlayers.slice(shuffledPlayers.length / 2)];
+function createRandomTeams(numTeams) {
+  const shuffledPlayers = playerNames.sort(() => 0.5 - Math.random());
+  const teamSize = Math.floor(shuffledPlayers.length / numTeams);
+
+  let teams = [];
+  for (let i = 0; i < numTeams; i++) {
+    teams.push(shuffledPlayers.slice(i * teamSize, (i + 1) * teamSize));
+  }
+  return teams;
 }
 
-function updateScore(player, round, points) {
-    // Assuming a win is 1 point and every scored point is 0.1
-    scores[playerNames.indexOf(player)] += points * 0.1;
-    calculateTotalScores();
-}
-
-function calculateTotalScores() {
-    const totalScoresDiv = document.getElementById('totalScores');
-    totalScoresDiv.innerHTML = '';
-    playerNames.forEach((player, index) => {
-        totalScoresDiv.innerHTML += `<div>${player}: ${scores[index]}</div>`;
-    });
-}
-
-document.getElementById('startGame').addEventListener('click', startGame);
+// ... rest of the code remains the same
